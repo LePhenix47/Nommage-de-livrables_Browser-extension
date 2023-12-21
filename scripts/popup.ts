7b6d5b8aaade02b38da028d58b528a43d603fc54
Younes Lahouiti <loller763@gmail.com>
2023-12-21 15:01:51 +0100

    Addition of code to later store the values on the web storage

--- scripts/popup.ts
+++ scripts/popup.ts
@@ -5,7 +5,16 @@ const form = selectQuery<HTMLFormElement>("form");
 form.addEventListener("submit", (e: SubmitEvent) => {
   e.preventDefault();
 
-  console.log(e);
+  const inputsArray = selectQueryAll<HTMLInputElement>("input");
+
+  const inputValuesMap = new Map<string, string | Date>();
+  for (const input of inputsArray) {
+    inputValuesMap.set(input.id, input.value);
+  }
+
+  console.log(inputValuesMap);
+
+  //    chrome.runtime.sendMessage({ type: 'formSubmission', data: formData });
 });
 
 console.log("Hello popup.ts", form);
